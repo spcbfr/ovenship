da9fc1c14b42dd7d11c40de6ac198076c1036ad0
Yusuf Bouzekri <yusufbouzekri@gmail.com>
2023-09-09 13:15:15 +0100

    Add essential integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), mdx()],
+  output: "server",
+  adapter: vercel()
+});
