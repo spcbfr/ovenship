13098d1f8c172924d52c5b1fc0c5f8b23ece5276
Yusuf Bouzekri <yusufbouzekri@gmail.com>
2023-09-21 20:17:38 +0100

    add code block support with skikhi

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,32 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import rehypePrettyCode from "rehype-pretty-code";
 
 import mdx from "@astrojs/mdx";
+// rehype-pretty-code configuration
+const prettyCodeOptions = {
+  theme: "github-light",
+  onVisitLine(node) {
+    if (node.children.length === 0) {
+      node.children = [
+        {
+          type: "text",
+          value: " ",
+        },
+      ];
+    }
+  },
+};
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
+  markdown: {
+    extendDefaultPlugins: true,
+    syntaxHighlight: false,
+    rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
+  },
   output: "server",
   adapter: vercel({
     analytics: true,
